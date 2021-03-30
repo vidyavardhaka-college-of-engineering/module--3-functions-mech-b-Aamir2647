c38b07a665240480f7c0259a78c49ba4db5812f6
Aamir2647 <20me0083@vvce.ac.in>
2021-03-30 15:25:18 +0000

    10.c ????

--- Program-10.c
+++ Program-10.c
@@ -11,9 +11,9 @@
 	int main()
 	{
 	    int var1,var2,var3;
-	    printf("enter two numbers");
+	    printf("enter two numbers ");
 	scanf("%d%d",&var1,&var2);
 	    var3=sub(var1,var2);
 	    printf("%d",var3);//complete the missing data
-	    return 0;
+	    return (0);
 	}
